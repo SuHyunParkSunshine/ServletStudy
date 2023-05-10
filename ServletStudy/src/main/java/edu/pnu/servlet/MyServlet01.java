6a58572b9d4fe8a66228e124032b6870c4d07128
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-05-10 17:59:58 +0900

    23.05.10_수업

--- ServletStudy/src/main/java/edu/pnu/servlet/MyServlet01.java
+++ ServletStudy/src/main/java/edu/pnu/servlet/MyServlet01.java
@@ -8,12 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MyServlet01 extends HttpServlet {
+	//HttpServlet이라는 부모로부터 상속을 받아야지만 호출이 가능하다
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
+	
+	//main이 실행이되는 이유는 톰캣이 알아서 찾아서 실행해주기 때문?
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
